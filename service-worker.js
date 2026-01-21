c11ffb8477ed8ff7be1bdb6652f11c9846e04ee4
tolstojm981-prog <tolstojm981@gmail.com>
2026-01-21 17:48:08 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Service Worker for P-DTR Complete PWA
+const CACHE_NAME = 'pdtr-v1';
+const urlsToCache = [
+  '/pdtr-app.html',
+  '/pdtr-cards-data.js',
+  '/pdtr-reference-data.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
